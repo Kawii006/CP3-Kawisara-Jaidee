1241cb26b8cf0b3e0ebab1dd6f6fa213f80f9f1d
Kawii006 <jkawisara@gmail.com>
2024-11-09 16:34:33 +0700

    Update Exercise4_Kawisara_J.py

--- Exercise4_Kawisara_J.py
+++ Exercise4_Kawisara_J.py
@@ -1,1 +1,9 @@
-
+a = int(input('Enter Foundation English : '))
+b = int(input('Enter General Business : '))
+c = int(input('Enter Introduction to Computer Systems :'))
+d = int(input('Enter Computer Progarmming : '))
+print('--- Your score ---')
+print('Foundation English :',a)
+print('General Business :',b)
+print('Introduction to Computer Systems :',c)
+print('Computer Progarmming :',d)

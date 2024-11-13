2aa799bd84d15a4dfaea9f1c9d9bea48101d273d
Kawii006 <jkawisara@gmail.com>
2024-11-13 14:53:27 +0700

    Create Exercise5_1_kawisara_J.py

---
+++ Exercise5_1_kawisara_J.py
@@ -0,0 +1,7 @@
+
+x = int(input('Enter number 1 = '))
+y = int(input('Enter number 2 = '))
+print(x ,'+', y ,'=', x + y)
+print(x ,'-', y ,'=', x - y)
+print(x ,'*', y ,'=', x * y)
+print(x ,'/', y ,'=', x / y)
